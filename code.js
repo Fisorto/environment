90c74e8f6070bb8bea3801f7636677c30f1ba2fb
Fisorto <42842653+Fisorto@users.noreply.github.com>
2022-01-27 17:05:01 +0200

    Add files via upload

--- code.js
+++ code.js
@@ -12,40 +12,11 @@ setInterval(
         ctx.clearRect(0, 0, 1100, 740);
         water();
         bottom();
-        // игровой цикл
-        for(let i = 0; i < arrayOfSpecies.length; i++){
-            if (arrayOfSpecies[i].alive === "yes")
-                arrayOfSpecies[i].drawFigure();
-                // проверка на жизнь и охотника
-            if (arrayOfSpecies[i].alive === "yes" && arrayOfSpecies[i].hunter === "no" ){
-                arrayOfSpecies[i].move();   
+        for(let i = 0; i < arraySpecies.length ;i++){
+            arraySpecies[i].drawFigure();
+            if(arraySpecies[i] instanceof Plants === false ){
+               arraySpecies[i].move(); 
             }
-            if(arrayOfSpecies[i].hunter === "yes"){
-                arrayOfSpecies[i].hunting(test2, test);
-            }
-
-
-            
-            //Проверка на охотника и поиск его жертвы, не доделан, я пока не знаю как это решить, и думаю в будущем найду решение
-            /*if (arrayOfSpecies[i].hunter === "yes") {
-                for (let j = 0; j < arrayOfSpecies.length; j++) {
-                    if (arrayOfSpecies[i] > arrayOfSpecies[j]) {
-                        if(Math.abs(arrayOfSpecies[j].x - arrayOfSpecies[i].x) < 50 && Math.abs( arrayOfSpecies[j].y - arrayOfSpecies[i].y) < 50){
-                            interactions.push([arrayOfSpecies[j], arrayOfSpecies[i]]);
-                        }
-                    }
-                }            
-            }*/
-            // удаление мертвецов
-            /*if (arrayOfSpecies[i].alive === "no"){
-                if (i === 0) {
-                    splice (arrayOfSpecies[i], arrayOfSpecies[i]);
-                } else {
-                    splice (arrayOfSpecies[i - 1], arrayOfSpecies[i]);
-                }
-                
-            }   */
-            
         }
 
         ctx.strokeRect(0, 0, 1100, 740);
